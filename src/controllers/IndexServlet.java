8c2025c273482589366d06c029ce3035612f3158
Hikaru Yoshino <yoshino@NOTE901.JPRS>
2020-09-07 16:58:22 +0900

    Index/NewServlet 404

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,16 +1,15 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Message;
 import utils.DBUtil;
 /**
  * Servlet implementation class IndexServlet
@@ -33,11 +32,15 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class)
-                                   .getResultList();
-        response.getWriter().append(Integer.valueOf(messages.size()).toString());
+        //List<Message> messages = em.createNamedQuery("getAllMessages", Message.class)
+        //                           .getResultList();
 
         em.close();
+
+        //request.setAttribute("messages", messages);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
+        rd.forward(request, response);
     }
 
 	/**
